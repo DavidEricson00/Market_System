3b2e9c8d6bb61e20c3eba45eb67fbee3cdcee611
David Ericson <davidestudofcg@gmail.com>
2024-09-27 15:12:00 -0300

    client base interface

--- client.py
+++ client.py
@@ -1,3 +1,48 @@
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def list_products():
+    print('List Products')
+
+def buy_product():
+    print('Update Product')
+
+def menu():
+    print('1. List Products')
+    print('2. Buy Product')
+    print('3. Exit')
+
+def clientheader():
+    print('-------------------------------------')
+    print('   Welcome to the Client interface   ')
+    print('-------------------------------------')
+
+def header():
+    print('------------------------------------')
+
 def main():
-    print('Under Construction')
-    print('This section will be developed soon. Please check back later!')
+    option = 0
+    while option != 3:
+        clientheader()
+        menu()
+        try:
+            option = int(input('Select the desired option: '))
+        except ValueError:
+            clear_screen()
+            print('Invalid input, please enter a number')
+            continue
+
+        if option == 1:
+            clear_screen()
+            list_products()
+        elif option == 2:
+            clear_screen()
+            buy_product()
+        elif option == 3:
+            clear_screen()
+            print('Exiting the Client Interface...')
+        else:
+            clear_screen()
+            print('Invalid option')
